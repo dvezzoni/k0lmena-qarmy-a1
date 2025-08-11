6104b037582ca72b48fd8c1aea9139a061f0074b
Underc0de <dinno.seven@gmail.com>
2025-08-11 19:16:16 -0300

    entrega examen

--- src/front-test/features/example.feature
+++ src/front-test/features/example.feature
@@ -1,6 +1,7 @@
-@MercadoLibreSearch @Smoke
-Feature: MercadoLibre search
-    Scenario: User search and validate results
-        Given User navigates to MercadoLibre page
-        When User search for cars options
-        Then It should show all the results according to the search
+@Smoke
+Feature: Versiones de K0lmena
+    Scenario: Verificar que se puedan ver las versiones de k0lmena
+        Given El usuario navega a la pagina de k0lmena
+        When El usuario clickea el link de Changelog
+        And El usuario clickea la version 2.0.0
+        Then El usuario ve la informacion de la version 2.0.0
--- src/front-test/steps/example.test.ts
+++ src/front-test/steps/example.test.ts
@@ -4,8 +4,8 @@ import { BASEURL } from '../config';
 import { pages } from '../hooks/hook';
 import { validateFirstLocator } from '../utils/validations';
 import {
-  inputLabel,
-  buttonSearch,
+  changelogLink,
+  version,
   divResult
 } from '../locators/exampleLocators';
 import {
@@ -14,22 +14,26 @@ import {
   getElementByRole
 } from '../utils/interactions';
 
-Given("User navigates to MercadoLibre page", async () => {
+Given("El usuario navega a la pagina de k0lmena", async () => {
   for (const page of pages) {
     console.log(`Ejecutando prueba en navegador: ${page.context().browser()?.browserType().name()}`);
     await page.goto(BASEURL);
   }
 });
 
-When('User search for cars options', async function () {
+When('El usuario clickea el link de Changelog', async function () {
   for (const page of pages) {
-    await getByPlaceholderAndClickIt(page, inputLabel);
-    await getByPlaceholderAndFillIt(page, inputLabel, "auto");
-    await page.locator(buttonSearch).click();
+    await page.getByText(changelogLink).click();
   }
 });
 
-Then('It should show all the results according to the search', async function () {
+When('El usuario clickea la version 2.0.0', async function () {
+  for (const page of pages) {
+    await page.getByRole('link', {name:version})
+  }
+});
+
+Then('El usuario ve la informacion de la version 2.0.0', async function () {
   for (const page of pages) {
     expect(validateFirstLocator(page, "div", divResult)).toBeTruthy();
   }
